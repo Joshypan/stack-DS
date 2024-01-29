4d5a6190b2e8d9aa912f0c0b570607ca76a7bd0f
Joshua Panchana <jpanchana0902@icloud.com>
2024-01-28 20:07:13 -0500

    Initial Commit

---
+++ stack-DS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  stack-DS
+//
+//  Created by Joshua Panchana Cedeño on 1/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
